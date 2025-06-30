d5b9c07b6b9df75b99f67ed9196d84a5b5d75b5f
Akula Chandrakiran <98520079+Chandrakiran462@users.noreply.github.com>
2025-06-30 06:21:47 +0000

    Variables and Assignments

---
+++ vanriables_and_assignments.py
@@ -0,0 +1,21 @@
+
+
+myvar=10    #valid var
+myVar=10    #valid var
+myvar1=10   #valid var
+my_var=10   #valid var
+# my@var=10   ***[SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?]***
+Myvar=10    #valid var
+#1myvar=10   ***[SyntaxError: invalid decimal literal]***
+_myvar=10   #valid var
+#@myvar=10  ***[SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?]
+#my var=10   ***[SyntaxError: invalid syntax]***
+a=10 
+a,b,c=10,20,30
+print(a,b,c)
+a=b=c=10
+print(a,b,c)
+a=10
+b=20
+a,b=b,a
+print(a,b)
