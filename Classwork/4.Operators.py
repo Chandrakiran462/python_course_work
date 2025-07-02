a26c6b42cd2ae788d45bc6d60e26841d8f484fbc
Akula Chandrakiran <98520079+Chandrakiran462@users.noreply.github.com>
2025-07-02 07:04:36 +0000

    Python Operators

--- Classwork/4.Operators.py
+++ Classwork/4.Operators.py
@@ -10,6 +10,47 @@ print("Division(/):",a/b)           # Division(/): 2.0
 print("floor Division(//):",a//b)   # floor Division(//): 2
 print("Modulus(%):",a%b)            # Modulus(%): 0
 print("Modulus(%):",b%a)            # Modulus(%): 10 [rerurns difference b/w a and b if b>a]
-print("Exponentiation(**):",a**b)    # Exponentiation(**): 10240000000000
+print("Exponentiation(**):",a**b)   # Exponentiation(**): 10240000000000
 
 # 2. Comparison Operators
+print("Equal to(==):",a==b)                     # Equal to(==): False [Checks if both values are equal]
+print("Not Equal to(!=):",a!=b)                 # Not Equal to(!=): True [Checks if values are not equal]
+print("Greater than(>):",a>b)                   # Greater than(>): True [Checks if the first value is greater]
+print("Greater than or Equal to(>=):",a>=b)     # Greater than or Equal to(>=): True [Checks if the first value is greater or equal]
+print("Less than(<):",a<b)                      # Less than(<): False [Checks if the first value is smaller]
+print("Less than or Equal to(<=):",a<=b)        # Less than or Equal to(<=): False [Checks if the first value is smaller or equal]
+
+# 3. Assignment Operators
+c = 6
+print("Assign(=):",c)                       # Assign(=): 6
+a+=b
+print("Add & Assign(+=):",a)                # Add & Assign(+=): 30
+a-=b
+print("Subtract & Assign(-=):",a)           # Subtract & Assign(-=): 20
+a*=b
+print("Multiply & Assign(*=):",a)           # Multiply & Assign(*=): 200
+a/=b
+print("Divide & Assign(/=):",a)             # Divide & Assign(/=): 20.0
+a//=b
+print("Floor Divide & Assign(//=):",a)      # Floor Divide & Assign(//=): 2.0
+a%=c
+print("Modulus & Assign(%=):",a)            # Modulus & Assign(%=): 2.0
+b**=c
+print("Exponentiate & Assign(**=):",b)      # Exponentiate & Assign(**=): 1000000
+
+# 4. Logical Operators (Using Logic Gates)
+print("AND(and):",a < 100 and c > 5)        # AND(and): True    [Returns True if both conditions are true]
+print("OR(or):",a < 100 or c > 100)         # OR(or): True      [Returns True if at least one condition is true]
+print("NOT(not):",not(a < b))               # NOT(not): False   [Reverses the condition (True becomes False)]
+
+# 5. Membership Operators
+s = "Kiran"
+print('i' in s)
+l = ["kiran","data","analytics"]
+print('kiran' in l)
+t = (1,4,3,6,7)
+print(8 not in t)
+se = {2,5,3,7}
+print(10 not in se)
+d = {'name':'kiran' , 'course' : 'DA'}
+print('batch' in d)
