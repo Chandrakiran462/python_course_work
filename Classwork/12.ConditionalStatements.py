388a2edb8b569cc034c3a99c5a8ed4fe26f7775a
Akula Chandrakiran <98520079+Chandrakiran462@users.noreply.github.com>
2025-07-10 07:02:05 +0000

    Python Conditional Statements

---
+++ Classwork/12.ConditionalStatements.py
@@ -0,0 +1,37 @@
+'''if-else Example'''
+# items = ['phone','airpods','earphones','laptop','watch']
+# print("Welcome to Amazon Store".center(50,'*'))
+# search = input('enter the item: ').lower()
+# if search in items:
+#     print(f"{search} found. Buy Now.")
+# else:
+#     print(f"{search} is out of stock. we will notify you.")
+
+
+'''if-elif-else example'''
+# weekend Budget plan
+# print("Weekend budget plan".center(50,'*'))
+# amount = int(input("Enter the amount you can spend: "))
+# if amount > 20000:
+#     print("Go to Goa")
+# elif amount > 15000:
+#     print("Go for Shopping")
+# elif amount > 10000:
+#     print("Clubbinggg")
+# elif amount > 5000:
+#     print("Go for Dinner") 
+# elif amount > 2000:
+#     print("Go for Box Cricket")
+# elif amount > 500:
+#     print("Go for Movie")
+# elif amount > 100:
+#     print("Go for street food")
+# else:
+#     print("save the money and sleep")
+
+
+'''Neseted if Example'''
+# Grade system
+data = {
+    1:{'name':'Kiran','attempt_status':True,'python':95,'sql':80,'powerbi':95},
+}
