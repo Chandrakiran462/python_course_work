939c08e614db1ca1f00fc95b0efcd955d13460a3
Chandra Kiran Akula <chandrakiran5292.com>
2025-07-11 18:07:08 +0530

    Zing Bus Ticket Booking

--- Assignments/Assignment1.py
+++ Assignments/Assignment1.py
@@ -1,1 +1,20 @@
-print("Assignment")
+#Zing Bus Ticket Booking
+
+Boarding_point = input("Enter Boarding point: ")
+Dropping Point = input("Enter Dropping Point: ")
+Date = input("Enter Date: ")
+berth = input("Enter Berth(upper or lower): ")
+bus = input("Enter Bus feature (AC or Non-AC): ")
+seat_type = input("Enter Seat Type(Seater or Sleeper): ")
+total_seats = int(input("Enter Total Seats: "))
+seats_booked = int(input("Enter No.of Seats Booked: "))
+seat_numbers = tuple(map(str,input("Enter Seat Number(s)(comma-seperated): ").split(',')))
+amenities = list(map(str,input("Enter Bus amenities(comma-seperated): ").split(',')))
+price = float(input("Enter Price: "))
+discount = float(input("Enter Discount Percentage: "))
+service_name = input()
+driver_name = input()
+driver_contact = input()
+
+
+print()
