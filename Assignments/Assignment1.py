d9516ab5e87ae37c80a2a5977859176287e8ac00
Chandrakiran462 <chandrakiran5292@gmail.com>
2025-07-11 18:27:55 +0530

    Zing Bus Ticket Booking

--- Assignments/Assignment1.py
+++ Assignments/Assignment1.py
@@ -19,4 +19,5 @@ driver_contact = input()
 
 print("Zing Bus Journey Details".center(50,'-'))
 print("Zing Bus Boarding Details".center(50,'-'))
+print("Zing Bus price Details".center(50,'-'))
 print("Zing Bus Driver Details".center(50,'-'))
