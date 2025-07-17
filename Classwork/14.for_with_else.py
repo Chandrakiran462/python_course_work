8e674ac7ea4fd9b3fb774e213f04b0613ed2f759
Chandrakiran462 <chandrakiran5292@gmail.com>
2025-07-17 12:38:26 +0530

    loops and pattrens

---
+++ Classwork/14.for_with_else.py
@@ -0,0 +1,5 @@
+l = ['phone','laptop','pods','earphones']
+for i in l:
+    print(i)
+else:
+    print("End of the list")
---
+++ Classwork/15.Nested_loops.py
@@ -0,0 +1,90 @@
+n = int(input("enter size: "))
+
+# for rows in range(n):
+#     for cols in range(n):
+#         print("*",end = " ")
+#     print()
+'''
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+'''
+
+# for rows in range(n):
+#     for cols in range(n):
+#         print(rows,end = " ")
+#     print()
+'''
+0 0 0 0 0 
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+'''
+
+# for rows in range(n):
+#     for cols in range(n):
+#         print(cols,end = " ")
+#     print()
+
+'''
+0 1 2 3 4 
+0 1 2 3 4
+0 1 2 3 4
+0 1 2 3 4
+0 1 2 3 4
+'''
+
+# for rows in range(n):
+#     for cols in range(n + 1):
+#         print(rows,end = " ")
+#     print()
+
+'''
+0
+1 1
+2 2 2
+3 3 3 3
+4 4 4 4 4
+'''
+# for rows in range(n):
+#     for cols in range(n - rows):
+#         print(rows,end = " ")
+#     print()
+
+'''
+enter size: 9
+0 0 0 0 0 0 0 0 0 
+1 1 1 1 1 1 1 1
+2 2 2 2 2 2 2
+3 3 3 3 3 3
+4 4 4 4 4
+5 5 5 5
+6 6 6
+7 7
+8
+'''
+
+# for rows in range(n):
+#     for space in range(n - rows -1):
+#         print(" ",end = " ")
+#     for cols in range(rows+1):
+#         print("*",end = " ")
+#     print()
+
+'''
+enter size: 5
+        * 
+      * *
+    * * *
+  * * * *
+* * * * *
+'''
+for rows in range(n):
+    for spaces in range(rows):
+        print(" ",end = " ")
+    for cols in range(n - rows):
+        print("*",end = " ")
+    print()
