b49a4ff686913f909f6bc9cc22009d15d4b33671
Chandrakiran462 <chandrakiran5292@gmail.com>
2025-07-21 11:23:12 +0530

    photo gallery

--- Classwork/task2.py
+++ Classwork/task2.py
@@ -17,3 +17,24 @@ selected=set(map(int,input("select photos to share(comma seperated): ").split(",
 print("sharing the following photos: ")
 for i in selected:
     print(photos[i])
+
+#o/p
+'''
+Photo Gallery: 
+1. beach.png
+2. birthday.jpg
+3. trip.jpg
+4. sunset.png
+5. Dinner.jpg
+6. Biryani.jpg
+7. sky.png
+8. moon.png
+9. stars.jpg
+select photos to share(comma seperated): 5,1,5,4,7,5,9,5
+sharing the following photos: 
+beach.png
+sunset.png
+Dinner.jpg
+sky.png
+stars.jpg
+'''
