68aec4a9d2bb963b23127d3d1a50d1b439a2ae4d
Chandrakiran462 <chandrakiran5292@gmail.com>
2025-08-06 10:59:57 +0530

    Python Modules

--- Classwork/atmlogic.py
+++ Classwork/atmlogic.py
@@ -9,15 +9,15 @@ data = {
 }
 acc_num = None
 login_status = None
-def welcome():
+def Welcome():
     print('Welcome to ATM'.center(50,'*'))
-def menu():
+def Menu():
     print('\n[C]heck_Balance')
     print('[D]eposite')
     print('[W]itdraw')
     print('[T]ransection_History')  
     print('[E]xit\n')
-def login():
+def Login():
     account_number = int(input("Enter Acctount Number: "))
     pin = int(input('Enter Pin number: '))
     global acc_num
@@ -32,3 +32,44 @@ def login():
         print('Invalid Accounnt Number')
         login_status = False
         return False
+def Check_balance():
+    if login_status and acc_num:
+        print(f'\nCurrent Balance: {data[acc_num]["balance"]}')
+    else:
+        print("You need to login again")
+
+
+def Deposit():
+    if login_status and acc_num:
+        amount=int(input("Enter the amount to deposit: "))
+        data[acc_num]['balance']+=amount
+        data[acc_num]['history'].append(f'Deposited - ${amount}')
+        print(f'${amount} is successfully desposited')
+    else:
+        print("You need to login again")
+
+def Withdraw():
+    if login_status and acc_num:
+        amount=int(input("Enter the amount to withdraw: "))
+        if data[acc_num]['balance']>=amount:
+            data[acc_num]['balance']-=amount
+            data[acc_num]['history'].append(f'Withdraw - ${amount}')
+            print(f'${amount} is successfully Withdraw')
+        else:
+            print("Insufficient Balance")
+    else:
+        print("You need to login again")
+
+def View_transaction():
+    if login_status and acc_num:
+        if data[acc_num]['history']:
+            print("Transactions History".center(50,'-'))
+            for i in data[acc_num]['history']:
+                print(i)
+            else:
+                print("End of the history".center(50,'-'))
+
+        else:
+            print("No Transactions")
+    else:
+        print("You need to login again")
--- Classwork/atmmain.py
+++ Classwork/atmmain.py
@@ -1,5 +1,20 @@
 import atmlogic as atm
+atm.Welcome()
 
-atm.welcome()
-atm.menu()
-atm.login()
+if atm.Login():
+    while True:
+        atm.Menu()
+        ch=input("Enter the choice: ").upper()
+        if ch=='C':
+            atm.Check_balance()
+        elif ch=='D':
+            atm.Deposit()
+        elif ch=='W':
+            atm.Withdraw()
+        elif ch=='V':
+            atm.View_transaction()
+        elif ch=='E':
+            print("Thankyou".center(50,'_'))
+            break
+        else:
+            print("Invalid Choice")
